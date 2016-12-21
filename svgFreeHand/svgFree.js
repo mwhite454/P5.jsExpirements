0e3a12c0f3bca9b9a590b0c0cfe86f5a7125167f
mwhite454 <mwhite454@gmail.com>
2016-12-21 15:06:02 -0600

    updating SVGFreeHand Structure

--- svgFreeHand/svgFree.js
+++ svgFreeHand/svgFree.js
@@ -11,11 +11,21 @@ var globals = {
 ///Event listeners
 window.addEventListener("load", function(){
   globals.currDraw = document.getElementById("svgPort").innerHTML;
+  //set svgPort width and height based on window size
+  setPortSize();
 });
 
 document.getElementById("svgPort").addEventListener("mousedown", startDraw);
 document.getElementById("svgPort").addEventListener("mouseup", endDraw);
 document.getElementById("svgPort").addEventListener("mousemove", collectPoints);
+
+function setPortSize(){
+  var port = document.getElementById("svgPort");
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  port.setAttribute("width", width-300);
+  port.setAttribute("height", height);
+}
 
 function startDraw(){
   globals.isDrawing = true;
@@ -31,20 +41,24 @@ function endDraw(){
       poly = poly + globals.points[i];
     }
   }
-  poly = "<polyline id='el" + globals.currentElement + "' points='" + poly + "' style='stroke:black; stroke-width:1; fill: none' />";
+  var el = "element" + globals.currentElement;
+  poly = "<polyline id='" + el + "' points='" + poly + "' style='stroke:black; stroke-width:1; fill: none' />";
   globals.drawing.push(globals.points);
+  updateElementTree(el);
   globals.points = [];
   document.getElementById("svgPort").innerHTML = globals.currDraw + poly;
   globals.currentElement +=1;
   globals.isDrawing = false;
+
 }
 
 function collectPoints(){
   if(globals.isDrawing){
     var point = " " + event.clientX + "," + event.clientY;
     globals.points.push(point);
+    refreshDraw();
   }
-  refreshDraw();
+
 }
 
 function refreshDraw(){
@@ -52,14 +66,15 @@ function refreshDraw(){
     var poly = thisDraw.getAttribute("points");
     var i = globals.points.length-1;
     if(globals.points.length<2){
-      poly = poly;
+      poly = " ";
+      poly = globals.points[0] + " " + globals.points[0];
     } else if(globals.points.length===2){
-      poly = "";
+      poly = " ";
       poly = globals.points[0] + " " + globals.points[1];
     } else {
-      for(var i=1; i<globals.points.length-1; i++){
+      //for(var i=1; i<globals.points.length-1; i++){
         poly = poly + globals.points[i];
-      }
+      //}
     }
     thisDraw.setAttribute("points", poly);
 }
